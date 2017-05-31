f133cf0ddecf1f5ef91166a4c7b6b26782dc1978
Doug L <douglubaway@gmail.com>
2017-05-31 09:50:36 -0700

    First two Khan Academy lessons finished.

--- README.md
+++ README.md
@@ -1,1 +1,111 @@
-# Relational-Databases
+## Specifications
+
+These are the basic specs for Relational Databases, broken into 5 stages. If you complete these specs, try taking on some of the [Stretch specs](#stretch).
+
+#### Stage 1
+
+Setup repo, complete a Khan Academy project, and install PostgreSQL. For instructions on installing PostgreSQL, see the guides in [Resources > Guides](#guides).
+
+- [X] Artifact repo is created on GitHub.
+- [X] The artifact repo is properly licensed, preferably with the [MIT license][mit-license].
+- [X] Solution SQL for the Khan Academy project: [Design a store database][ka-proj-store-db] is added to a file in your repo named `store_db.sql`
+- [X] SQL in `store_db.sql` has consistent indentation.
+- [X] All the SQL keywords in `store_db.sql` are written in ALL CAPS.
+- [X] PostgreSQL is installed on your computer with Homebrew.
+
+#### Stage 2
+
+Complete another Khan Academy project, use the SQL Designer tool, and install the [Postico app][postico] (this is recommended, but not required).
+
+- [ ] Solution SQL for the Khan Academy project: [Data dig][ka-proj-data-dig] is added to a file in your repo named `data_dig.sql`
+- [ ] SQL in `data_dig.sql` has consistent indentation.
+- [ ] All the SQL keywords in `data_dig.sql` are written in ALL CAPS.
+- [ ] Schema diagrams are created for both the "Store database" and "Data dig" database using the [SQL Designer tool][sql-designer].
+- [ ] Screenshots of each schema diagram are added to repo as `store_db_schema.png` and `data_dig_schema.png`, respectively.
+
+#### Stage 3
+
+Create schemas and mock data for the "Music Player" app from the Init 3 goal. Then run queries against them. Read the [Generating Mock Data](#generating-mock-data) section to learn how to generate fake data.
+
+##### Music Player
+
+- [ ] Folder `music-player/` exists in your repo.
+- [ ] Database schema SQL (using PostgreSQL syntax) for the Music Player from [Init 3: Hello, Web Servers][hello-web-servers] is included in the file `music-player/schema.sql`.
+- [ ] Music Player schema includes table and appropriate columns for Artists
+- [ ] Music Player schema includes table and appropriate columns for Albums
+- [ ] Music Player schema includes table and appropriate columns for Songs
+- [ ] Music Player schema includes table and appropriate columns for Playlists
+- [ ] Mock data is generated for the Music Player and added to the file `music-player/mock_data.sql`.
+- [ ] Sample queries against the Music Player database are added to the file `music-player/queries.sql`.
+- [ ] Queries against the Music Player use the `WHERE` keyword (at least once)
+- [ ] Queries against the Music Player use the `LIMIT` keyword (at least once)
+- [ ] Queries against the Music Player use the `ORDER BY` keyword (at least once)
+- [ ] Queries against the Music Player use the `LIKE` keyword (at least once)
+- [ ] Queries against the Music Player use the `AND/OR` keyword (at least once)
+- [ ] Queries against the Music Player use the `HAVING` keyword (at least once)
+- [ ] Queries against the Music Player use the `IN` keyword (at least once)
+- [ ] Queries against the Music Player use the `JOIN` keyword (at least once)
+
+##### Bike Shop
+
+Create a database for a fictional bike shop. Use the [Table Schema for Bike Shop](#table-schema-for-bike-shop) provided.
+
+- [ ] Folder `bike-shop/` exists in your repo.
+- [ ] Database schema SQL (using PostgreSQL syntax) for the Bike Shop is included in the file `bike-shop/schema.sql`.
+- [ ] Bike Shop schema matches the specifications in [Table Schema for Bike Shop](#table-schema-for-bike-shop).
+
+#### Stage 4
+
+Complete the final Khan Academy project. Design and create schemas for the "Bossggle" app from the Init 1 goals.
+
+- [ ] Solution SQL for the Khan Academy project: [App impersonator][ka-proj-app-impersonator] is added to a file in your repo named `app_impersonator.sql`
+- [ ] SQL in `app_impersonator.sql` has consistent indentation.
+- [ ] All the SQL keywords in `app_impersonator.sql` are written in ALL CAPS.
+- [ ] Folder `bossggle/` exists in your repo.
+- [ ] Database schema SQL (using PostgreSQL syntax) for the Bossggle game from [Init 1: Bossggle Word Game][bossggle-word-game] is included in the file `bossggle/schema.sql`.
+
+Bossggle schema supports the following features (i.e. you _could_ write SQL to show this data):
+- [ ] List all words guessed
+- [ ] Count all correct words guessed
+- [ ] Count all incorrect words guessed
+- [ ] List all words guessed and their scores
+- [ ] Get sum of scores for all words guessed
+- [ ] Show 10 most recently guessed words
+
+#### Stage 5
+(Facebook and Reddit?)
+Pick 2-3 real-world apps (for example: Twitter, Instagram, Google Drive, etc.) and design a sample database schema for each of them. Then generate some mock data and run queries against them. Your schemas don't have to be _exactly_ what these apps would use, but they should roughly approximate the basic features.
+
+- [ ] 2 or 3 real-world apps are chosen and folders are created for each of them.
+
+For each real-world app chosen there exists:
+- [ ] A SQL schema in the file `<app name>/schema.sql`.
+- [ ] A set of mock data `INSERT` statements in the file `<app name>/mock_data.sql`.
+- [ ] A set of example queries in the file `<app name>/queries.sql`.
+- [ ] Queries against the real-world apps use the `INSERT` keyword (at least once)
+- [ ] Queries against the real-world apps use the `UPDATE` keyword (at least once)
+- [ ] Queries against the real-world apps use the `DELETE` keyword (at least once)
+- [ ] Queries against the real-world apps use the `WHERE` keyword (at least once)
+- [ ] Queries against the real-world apps use the `LIMIT` keyword (at least once)
+- [ ] Queries against the real-world apps use the `ORDER BY` keyword (at least once)
+- [ ] Queries against the real-world apps use the `JOIN` keyword (at least once)
+- [ ] SQL in all real-world app SQL files have consistent indentation.
+- [ ] All the SQL keywords all real-world app SQL files are written in ALL CAPS.
+
+### Stretch
+
+If you complete all of the specs listed above (the checkboxes), there's no reason to stop there! Keep practicing with these stretch specs.
+
+Choose an additional 3 real-world apps to reverse-engineer the databases for.
+
+For each real-world app chosen there exists:
+- [ ] A folder in your repo `<app name>/`
+- [ ] A SQL schema in the file `<app name>/schema.sql`.
+- [ ] A set of mock data `INSERT` statements in the file `<app name>/mock_data.sql`.
+- [ ] A set of example queries in the file `<app name>/queries.sql`.
+
+Find a couple of large datasets online, create databases for them, and practice querying against them. Some good places to look are at [data.gov](https://www.data.gov/) and this list of [public datasets](https://github.com/caesar0301/awesome-public-datasets).
+
+- [ ] 2 large datasets are included as CSV or JSON data in a folder called `datasets/`.
+- [ ] SQL schema creating tables for the datasets are inclued in your repo.
+- [ ] 10+ queries against the datasets are included in files in your repo.
---
+++ data_dig.sql
@@ -0,0 +1,147 @@
+CREATE TABLE mock_user_data (
+			 id INTEGER,
+			 first_name TEXT,
+			 gender TEXT,
+			 hometown TEXT,
+			 birth_date TEXT,
+			 lucky_number INTEGER,
+			 cash INTEGER
+);
+INSERT INTO mock_user_data (id, first_name, gender, hometown, birth_date, lucky_number, cash) VALUES (1, 'Jacquette', 'Female', 'Erong', '4/29/1996', 30, 177);
+INSERT INTO mock_user_data (id, first_name, gender, hometown, birth_date, lucky_number, cash) VALUES (2, 'Lyndsie', 'Female', 'Loures', '10/23/1991', 84, 422);
+INSERT INTO mock_user_data (id, first_name, gender, hometown, birth_date, lucky_number, cash) VALUES (3, 'Irwin', 'Male', 'Itaperuçu', '11/23/1991', 30, 417);
+INSERT INTO mock_user_data (id, first_name, gender, hometown, birth_date, lucky_number, cash) VALUES (4, 'Ludwig', 'Male', 'Gyumri', '11/25/1994', 30, 156);
+INSERT INTO mock_user_data (id, first_name, gender, hometown, birth_date, lucky_number, cash) VALUES (5, 'Griff', 'Male', 'Ágios Spyrídon', '9/25/1994', 88, 390);
+INSERT INTO mock_user_data (id, first_name, gender, hometown, birth_date, lucky_number, cash) VALUES (6, 'Duffy', 'Male', 'Limoges', '12/30/1991', 75, 175);
+INSERT INTO mock_user_data (id, first_name, gender, hometown, birth_date, lucky_number, cash) VALUES (7, 'Robyn', 'Female', 'George Hill', '1/16/1992', 50, 126);
+INSERT INTO mock_user_data (id, first_name, gender, hometown, birth_date, lucky_number, cash) VALUES (8, 'Akim', 'Male', 'Bridel', '1/22/1997', 57, 173);
+INSERT INTO mock_user_data (id, first_name, gender, hometown, birth_date, lucky_number, cash) VALUES (9, 'Halsy', 'Male', 'Flic en Flac', '10/8/1993', 16, 69);
+INSERT INTO mock_user_data (id, first_name, gender, hometown, birth_date, lucky_number, cash) VALUES (10, 'Miguel', 'Male', 'Bailadores', '8/4/1996', 77, 275);
+INSERT INTO mock_user_data (id, first_name, gender, hometown, birth_date, lucky_number, cash) VALUES (11, 'Iggie', 'Male', 'Kuršėnai', '2/5/1992', 84, 371);
+INSERT INTO mock_user_data (id, first_name, gender, hometown, birth_date, lucky_number, cash) VALUES (12, 'Clayborn', 'Male', 'Linhares', '6/21/1998', 98, 67);
+INSERT INTO mock_user_data (id, first_name, gender, hometown, birth_date, lucky_number, cash) VALUES (13, 'Rivkah', 'Female', 'Bantarpanjang', '1/24/1995', 4, 261);
+INSERT INTO mock_user_data (id, first_name, gender, hometown, birth_date, lucky_number, cash) VALUES (14, 'Eryn', 'Female', 'Žacléř', '9/29/1992', 89, 163);
+INSERT INTO mock_user_data (id, first_name, gender, hometown, birth_date, lucky_number, cash) VALUES (15, 'Adair', 'Male', 'Canindé', '1/9/1995', 73, 82);
+INSERT INTO mock_user_data (id, first_name, gender, hometown, birth_date, lucky_number, cash) VALUES (16, 'Jecho', 'Male', 'Arapiraca', '3/31/1997', 34, 390);
+INSERT INTO mock_user_data (id, first_name, gender, hometown, birth_date, lucky_number, cash) VALUES (17, 'Dynah', 'Female', 'Antonina', '11/28/1997', 28, 267);
+INSERT INTO mock_user_data (id, first_name, gender, hometown, birth_date, lucky_number, cash) VALUES (18, 'Danya', 'Female', 'Cavan', '9/21/1991', 99, 169);
+INSERT INTO mock_user_data (id, first_name, gender, hometown, birth_date, lucky_number, cash) VALUES (19, 'Flossi', 'Female', 'Radenka', '11/14/1995', 7, 202);
+INSERT INTO mock_user_data (id, first_name, gender, hometown, birth_date, lucky_number, cash) VALUES (20, 'Caldwell', 'Male', 'Sopron', '6/7/1996', 69, 333);
+INSERT INTO mock_user_data (id, first_name, gender, hometown, birth_date, lucky_number, cash) VALUES (21, 'Somerset', 'Male', 'Wanling', '5/1/2000', 37, 167);
+INSERT INTO mock_user_data (id, first_name, gender, hometown, birth_date, lucky_number, cash) VALUES (22, 'Donetta', 'Female', 'Lac-Mégantic', '12/15/1996', 99, 415);
+INSERT INTO mock_user_data (id, first_name, gender, hometown, birth_date, lucky_number, cash) VALUES (23, 'Kevon', 'Male', 'Tarub', '11/6/1996', 55, 491);
+INSERT INTO mock_user_data (id, first_name, gender, hometown, birth_date, lucky_number, cash) VALUES (24, 'Arleta', 'Female', 'Baturaden', '10/9/1995', 40, 145);
+INSERT INTO mock_user_data (id, first_name, gender, hometown, birth_date, lucky_number, cash) VALUES (25, 'Nicky', 'Female', 'Belang', '6/19/1997', 59, 466);
+INSERT INTO mock_user_data (id, first_name, gender, hometown, birth_date, lucky_number, cash) VALUES (26, 'Roderich', 'Male', 'Wenquan', '11/30/1998', 32, 400);
+INSERT INTO mock_user_data (id, first_name, gender, hometown, birth_date, lucky_number, cash) VALUES (27, 'Bron', 'Male', 'Malasin', '8/19/1995', 47, 387);
+INSERT INTO mock_user_data (id, first_name, gender, hometown, birth_date, lucky_number, cash) VALUES (28, 'Jackie', 'Female', 'Santiago de las Vegas', '12/19/1993', 1, 179);
+INSERT INTO mock_user_data (id, first_name, gender, hometown, birth_date, lucky_number, cash) VALUES (29, 'Abie', 'Male', 'Sanjiang', '1/25/1993', 35, 194);
+INSERT INTO mock_user_data (id, first_name, gender, hometown, birth_date, lucky_number, cash) VALUES (30, 'Becky', 'Female', 'Chiconi', '9/7/1998', 51, 330);
+INSERT INTO mock_user_data (id, first_name, gender, hometown, birth_date, lucky_number, cash) VALUES (31, 'Bernadina', 'Female', 'Xinpeicun', '6/14/1997', 8, 342);
+INSERT INTO mock_user_data (id, first_name, gender, hometown, birth_date, lucky_number, cash) VALUES (32, 'Chere', 'Female', 'Ponta Porã', '3/12/1999', 25, 239);
+INSERT INTO mock_user_data (id, first_name, gender, hometown, birth_date, lucky_number, cash) VALUES (33, 'Niven', 'Male', 'Turets-Bayary', '5/6/1996', 54, 58);
+INSERT INTO mock_user_data (id, first_name, gender, hometown, birth_date, lucky_number, cash) VALUES (34, 'Lewiss', 'Male', 'Coyah', '2/17/1992', 25, 203);
+INSERT INTO mock_user_data (id, first_name, gender, hometown, birth_date, lucky_number, cash) VALUES (35, 'Broddy', 'Male', 'Strawczyn', '7/13/1999', 81, 256);
+INSERT INTO mock_user_data (id, first_name, gender, hometown, birth_date, lucky_number, cash) VALUES (36, 'Kristofor', 'Male', 'Shuangkou', '12/15/1997', 64, 171);
+INSERT INTO mock_user_data (id, first_name, gender, hometown, birth_date, lucky_number, cash) VALUES (37, 'Armin', 'Male', 'Rāvar', '5/6/1994', 58, 30);
+INSERT INTO mock_user_data (id, first_name, gender, hometown, birth_date, lucky_number, cash) VALUES (38, 'Zebulen', 'Male', 'Takaoka', '10/17/1991', 26, 377);
+INSERT INTO mock_user_data (id, first_name, gender, hometown, birth_date, lucky_number, cash) VALUES (39, 'Spencer', 'Male', 'Birigui', '2/18/1999', 40, 471);
+INSERT INTO mock_user_data (id, first_name, gender, hometown, birth_date, lucky_number, cash) VALUES (40, 'Jock', 'Male', 'Kónitsa', '4/4/1993', 86, 338);
+INSERT INTO mock_user_data (id, first_name, gender, hometown, birth_date, lucky_number, cash) VALUES (41, 'Julio', 'Male', 'Castellon De La Plana/Castello De La Pla', '6/15/1993', 27, 395);
+INSERT INTO mock_user_data (id, first_name, gender, hometown, birth_date, lucky_number, cash) VALUES (42, 'Karil', 'Female', 'Niños Heroes', '9/25/1999', 74, 75);
+INSERT INTO mock_user_data (id, first_name, gender, hometown, birth_date, lucky_number, cash) VALUES (43, 'Karena', 'Female', 'Yuta', '6/7/1992', 81, 303);
+INSERT INTO mock_user_data (id, first_name, gender, hometown, birth_date, lucky_number, cash) VALUES (44, 'Vikky', 'Female', 'Xihongmen', '2/7/1996', 40, 61);
+INSERT INTO mock_user_data (id, first_name, gender, hometown, birth_date, lucky_number, cash) VALUES (45, 'Tally', 'Male', 'Pakisaji', '6/5/1995', 71, 417);
+INSERT INTO mock_user_data (id, first_name, gender, hometown, birth_date, lucky_number, cash) VALUES (46, 'Mireille', 'Female', 'Strážnice', '2/25/2000', 53, 420);
+INSERT INTO mock_user_data (id, first_name, gender, hometown, birth_date, lucky_number, cash) VALUES (47, 'Jannelle', 'Female', 'Mannheim', '5/14/1999', 5, 409);
+INSERT INTO mock_user_data (id, first_name, gender, hometown, birth_date, lucky_number, cash) VALUES (48, 'Merilee', 'Female', 'Asunción Mita', '5/23/1999', 61, 399);
+INSERT INTO mock_user_data (id, first_name, gender, hometown, birth_date, lucky_number, cash) VALUES (49, 'Dilly', 'Male', 'Stara Błotnica', '12/10/1995', 85, 164);
+INSERT INTO mock_user_data (id, first_name, gender, hometown, birth_date, lucky_number, cash) VALUES (50, 'Elmo', 'Male', 'Caratinga', '2/19/1994', 43, 496);
+INSERT INTO mock_user_data (id, first_name, gender, hometown, birth_date, lucky_number, cash) VALUES (51, 'Richardo', 'Male', 'Wadungasri', '10/13/1992', 30, 385);
+INSERT INTO mock_user_data (id, first_name, gender, hometown, birth_date, lucky_number, cash) VALUES (52, 'Alasdair', 'Male', 'Grazhdanskoye', '6/20/1996', 93, 394);
+INSERT INTO mock_user_data (id, first_name, gender, hometown, birth_date, lucky_number, cash) VALUES (53, 'Patin', 'Male', 'Padurung', '11/2/1996', 95, 351);
+INSERT INTO mock_user_data (id, first_name, gender, hometown, birth_date, lucky_number, cash) VALUES (54, 'Perla', 'Female', 'Wang Noi', '4/12/1996', 33, 348);
+INSERT INTO mock_user_data (id, first_name, gender, hometown, birth_date, lucky_number, cash) VALUES (55, 'Had', 'Male', 'Örebro', '11/25/1995', 41, 257);
+INSERT INTO mock_user_data (id, first_name, gender, hometown, birth_date, lucky_number, cash) VALUES (56, 'Timothee', 'Male', 'Haolibao', '11/27/1998', 18, 406);
+INSERT INTO mock_user_data (id, first_name, gender, hometown, birth_date, lucky_number, cash) VALUES (57, 'Gilda', 'Female', 'Alor Setar', '12/27/1993', 93, 87);
+INSERT INTO mock_user_data (id, first_name, gender, hometown, birth_date, lucky_number, cash) VALUES (58, 'Lisle', 'Male', 'Bendosewu', '6/8/1999', 29, 215);
+INSERT INTO mock_user_data (id, first_name, gender, hometown, birth_date, lucky_number, cash) VALUES (59, 'Stearn', 'Male', 'Caeté', '2/5/1994', 63, 169);
+INSERT INTO mock_user_data (id, first_name, gender, hometown, birth_date, lucky_number, cash) VALUES (60, 'Percival', 'Male', 'Jiangkou', '3/5/2000', 83, 358);
+INSERT INTO mock_user_data (id, first_name, gender, hometown, birth_date, lucky_number, cash) VALUES (61, 'Gustavus', 'Male', 'Nusajaya', '11/22/1999', 35, 433);
+INSERT INTO mock_user_data (id, first_name, gender, hometown, birth_date, lucky_number, cash) VALUES (62, 'Elise', 'Female', 'Sanhe', '3/22/1992', 70, 85);
+INSERT INTO mock_user_data (id, first_name, gender, hometown, birth_date, lucky_number, cash) VALUES (63, 'Ancell', 'Male', 'Novyy Svit', '8/1/1997', 85, 54);
+INSERT INTO mock_user_data (id, first_name, gender, hometown, birth_date, lucky_number, cash) VALUES (64, 'Bron', 'Male', 'Tete', '5/14/1993', 100, 469);
+INSERT INTO mock_user_data (id, first_name, gender, hometown, birth_date, lucky_number, cash) VALUES (65, 'Bertie', 'Female', 'Mazatenango', '11/15/1998', 83, 111);
+INSERT INTO mock_user_data (id, first_name, gender, hometown, birth_date, lucky_number, cash) VALUES (66, 'Colman', 'Male', 'El Matama', '3/26/1992', 14, 71);
+INSERT INTO mock_user_data (id, first_name, gender, hometown, birth_date, lucky_number, cash) VALUES (67, 'Gorden', 'Male', 'Bagahanlad', '5/9/1994', 11, 144);
+INSERT INTO mock_user_data (id, first_name, gender, hometown, birth_date, lucky_number, cash) VALUES (68, 'Renee', 'Female', 'Saint-Laurent-Blangy', '2/27/1992', 22, 67);
+INSERT INTO mock_user_data (id, first_name, gender, hometown, birth_date, lucky_number, cash) VALUES (69, 'Nicolea', 'Female', 'Al Khafsah', '5/22/1995', 43, 49);
+INSERT INTO mock_user_data (id, first_name, gender, hometown, birth_date, lucky_number, cash) VALUES (70, 'Modestia', 'Female', 'Kansas City', '3/11/1995', 22, 318);
+INSERT INTO mock_user_data (id, first_name, gender, hometown, birth_date, lucky_number, cash) VALUES (71, 'Stinky', 'Male', 'Hengli', '2/13/1995', 51, 240);
+INSERT INTO mock_user_data (id, first_name, gender, hometown, birth_date, lucky_number, cash) VALUES (72, 'Morse', 'Male', 'Cayna', '3/19/1996', 69, 130);
+INSERT INTO mock_user_data (id, first_name, gender, hometown, birth_date, lucky_number, cash) VALUES (73, 'Germaine', 'Male', 'Lisui', '3/2/2000', 77, 323);
+INSERT INTO mock_user_data (id, first_name, gender, hometown, birth_date, lucky_number, cash) VALUES (74, 'Mandie', 'Female', 'Kampungtengah', '5/15/1995', 20, 346);
+INSERT INTO mock_user_data (id, first_name, gender, hometown, birth_date, lucky_number, cash) VALUES (75, 'Gaye', 'Female', 'Rembes', '1/18/1992', 95, 239);
+INSERT INTO mock_user_data (id, first_name, gender, hometown, birth_date, lucky_number, cash) VALUES (76, 'Odilia', 'Female', 'Ambositra', '11/19/1995', 74, 306);
+INSERT INTO mock_user_data (id, first_name, gender, hometown, birth_date, lucky_number, cash) VALUES (77, 'Humfrid', 'Male', 'Manhiça', '3/26/1992', 17, 273);
+INSERT INTO mock_user_data (id, first_name, gender, hometown, birth_date, lucky_number, cash) VALUES (78, 'Bobinette', 'Female', 'Werota', '4/29/1998', 30, 359);
+INSERT INTO mock_user_data (id, first_name, gender, hometown, birth_date, lucky_number, cash) VALUES (79, 'Adina', 'Female', '‘Ayn al Bayḑā', '4/27/1993', 77, 336);
+INSERT INTO mock_user_data (id, first_name, gender, hometown, birth_date, lucky_number, cash) VALUES (80, 'Verney', 'Male', 'Santa Marta de Penaguião', '12/20/1993', 59, 474);
+INSERT INTO mock_user_data (id, first_name, gender, hometown, birth_date, lucky_number, cash) VALUES (81, 'Randi', 'Female', 'Trnovlje pri Celju', '12/8/1998', 89, 329);
+INSERT INTO mock_user_data (id, first_name, gender, hometown, birth_date, lucky_number, cash) VALUES (82, 'Garth', 'Male', 'Pentéli', '2/7/1992', 52, 343);
+INSERT INTO mock_user_data (id, first_name, gender, hometown, birth_date, lucky_number, cash) VALUES (83, 'Briggs', 'Male', 'Larnaca', '12/5/1992', 81, 156);
+INSERT INTO mock_user_data (id, first_name, gender, hometown, birth_date, lucky_number, cash) VALUES (84, 'Rosabella', 'Female', 'Zoetermeer', '3/19/1994', 98, 131);
+INSERT INTO mock_user_data (id, first_name, gender, hometown, birth_date, lucky_number, cash) VALUES (85, 'Bald', 'Male', 'Karuri', '2/28/1995', 15, 359);
+INSERT INTO mock_user_data (id, first_name, gender, hometown, birth_date, lucky_number, cash) VALUES (86, 'Nelli', 'Female', 'Egvekinot', '1/11/2000', 80, 109);
+INSERT INTO mock_user_data (id, first_name, gender, hometown, birth_date, lucky_number, cash) VALUES (87, 'Homerus', 'Male', 'Mỹ Thọ', '7/1/1992', 3, 142);
+INSERT INTO mock_user_data (id, first_name, gender, hometown, birth_date, lucky_number, cash) VALUES (88, 'Janean', 'Female', 'Ängelholm', '12/23/1991', 35, 275);
+INSERT INTO mock_user_data (id, first_name, gender, hometown, birth_date, lucky_number, cash) VALUES (89, 'Upton', 'Male', 'Palmerston North', '10/25/1991', 24, 410);
+INSERT INTO mock_user_data (id, first_name, gender, hometown, birth_date, lucky_number, cash) VALUES (90, 'Lindi', 'Female', 'Rancabugel', '6/24/1991', 77, 423);
+INSERT INTO mock_user_data (id, first_name, gender, hometown, birth_date, lucky_number, cash) VALUES (91, 'Laura', 'Female', 'Moycullen', '4/11/1997', 90, 469);
+INSERT INTO mock_user_data (id, first_name, gender, hometown, birth_date, lucky_number, cash) VALUES (92, 'Marcella', 'Female', 'Bataiporã', '6/6/1992', 82, 254);
+INSERT INTO mock_user_data (id, first_name, gender, hometown, birth_date, lucky_number, cash) VALUES (93, 'Ailey', 'Female', 'Hexiangqiao', '1/30/1998', 55, 90);
+INSERT INTO mock_user_data (id, first_name, gender, hometown, birth_date, lucky_number, cash) VALUES (94, 'Ava', 'Female', 'São João da Barra', '10/10/1992', 100, 41);
+INSERT INTO mock_user_data (id, first_name, gender, hometown, birth_date, lucky_number, cash) VALUES (95, 'Daron', 'Male', 'Chunjiang', '1/2/1997', 99, 82);
+INSERT INTO mock_user_data (id, first_name, gender, hometown, birth_date, lucky_number, cash) VALUES (96, 'Tallou', 'Female', 'Capellanía', '2/20/1994', 84, 175);
+INSERT INTO mock_user_data (id, first_name, gender, hometown, birth_date, lucky_number, cash) VALUES (97, 'Coraline', 'Female', 'São Leopoldo', '10/21/1991', 73, 171);
+INSERT INTO mock_user_data (id, first_name, gender, hometown, birth_date, lucky_number, cash) VALUES (98, 'Mirelle', 'Female', 'Malaruhatan', '7/29/1999', 69, 249);
+INSERT INTO mock_user_data (id, first_name, gender, hometown, birth_date, lucky_number, cash) VALUES (99, 'Jacquelyn', 'Female', 'San Jose', '5/2/1994', 67, 163);
+INSERT INTO mock_user_data (id, first_name, gender, hometown, birth_date, lucky_number, cash) VALUES (100, 'Myrna', 'Female', '''s-Hertogenbosch', '3/2/1994', 63, 156);
+
+SELECT MAX(cash)
+  FROM mock_user_data;
+
+SELECT MIN(cash)
+  FROM mock_user_data;
+
+SELECT ROUND(AVG(cash))
+  FROM mock_user_data;
+
+SELECT first_name, lucky_number AS lucky_total
+	FROM mock_user_data
+ GROUP BY first_name
+HAVING first_name LIKE "K%";
+
+SELECT COUNT(*),
+	 CASE
+			WHEN cash > 200 THEN "Has more than 100 Dollars"
+			WHEN cash > 100 THEN "Has around 100 Dollars"
+			WHEN cash = 100 THEN "Has 100 Dollars"
+			ELSE "Has less than 100 Dollars"
+  	END	AS "Do_You_Have_100_Dollars"
+	FROM mock_user_data
+ GROUP BY Do_You_Have_100_Dollars;
+
+SELECT first_name
+	FROM mock_user_data
+ WHERE first_name
+	LIKE "T%"
+	 AND first_name
+	LIKE "%A%";
+
+SELECT first_name
+	FROM mock_user_data
+ WHERE first_name
+	LIKE "A%"
+		OR first_name
+	LIKE "T%";
