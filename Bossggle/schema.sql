6a21f442625c5df27e5d08a12cdd79b122f91ae9
Doug L <douglubaway@gmail.com>
2017-06-02 16:41:46 -0700

    Fixed Bossggle SELECT statements

--- Bossggle/schema.sql
+++ Bossggle/schema.sql
@@ -1,44 +1,57 @@
 CREATE TABLE bossggle (
-       id INTEGER PRIMARY KEY,
+       id SERIAL INTEGER PRIMARY KEY,
        words_guessed TEXT,
-       words_correct INTEGER,
-       words_incorrect INTEGER,
+       words_correct BOOLEAN,
        words_score INTEGER
 );
 
-INSERT INTO bossggle (words_guessed, words_correct, words_incorrect, words_score)
-VALUES ("ROAR", 1, 0, 36);
-INSERT INTO bossggle (words_guessed, words_correct, words_incorrect, words_score)
-VALUES ("ARJWOWRPOKPKWEF", 1, 1, 0);
-INSERT INTO bossggle (words_guessed, words_correct, words_incorrect, words_score)
-VALUES ("LADYBUG", 2, 1, 63);
-INSERT INTO bossggle (words_guessed, words_correct, words_incorrect, words_score)
-VALUES ("JOSH", 3, 1, 36);
-INSERT INTO bossggle (words_guessed, words_correct, words_incorrect, words_score)
-VALUES ("TYPO", 4, 1, 36);
-INSERT INTO bossggle (words_guessed, words_correct, words_incorrect, words_score)
-VALUES ("TYPE", 5, 1, 36);
-INSERT INTO bossggle (words_guessed, words_correct, words_incorrect, words_score)
-VALUES ("HORSE", 6, 1, 36);
-INSERT INTO bossggle (words_guessed, words_correct, words_incorrect, words_score)
-VALUES ("LION", 7, 1, 36);
-INSERT INTO bossggle (words_guessed, words_correct, words_incorrect, words_score)
-VALUES ("TORGUE", 8, 1, 56);
-INSERT INTO bossggle (words_guessed, words_correct, words_incorrect, words_score)
-VALUES ("TELLTALE", 9, 1, 72);
-INSERT INTO bossggle (words_guessed, words_correct, words_incorrect, words_score)
-VALUES ("HI", 9, 2, 0);
-INSERT INTO bossggle (words_guessed, words_correct, words_incorrect, words_score)
-VALUES ("BLEH", 10, 2, 36);
-INSERT INTO bossggle (words_guessed, words_correct, words_incorrect, words_score)
-VALUES ("ARE", 11, 2, 27);
-INSERT INTO bossggle (words_guessed, words_correct, words_incorrect, words_score)
-VALUES ("INTELLIGENCE", 12, 2, 108);
+INSERT INTO bossggle (words_guessed, words_correct, words_score)
+VALUES ("ROAR", "TRUE", 36);
+INSERT INTO bossggle (words_guessed, words_correct, words_score)
+VALUES ("ARJWOWRPOKPKWEF", "FALSE", 0);
+INSERT INTO bossggle (words_guessed, words_correct, words_score)
+VALUES ("LADYBUG", "TRUE", 63);
+INSERT INTO bossggle (words_guessed, words_correct, words_score)
+VALUES ("JOSH", "TRUE", 36);
+INSERT INTO bossggle (words_guessed, words_correct, words_score)
+VALUES ("TYPO", "TRUE", 36);
+INSERT INTO bossggle (words_guessed, words_correct, words_score)
+VALUES ("TYPE", "TRUE", 36);
+INSERT INTO bossggle (words_guessed, words_correct, words_score)
+VALUES ("HORSE", "TRUE", 36);
+INSERT INTO bossggle (words_guessed, words_correct, words_score)
+VALUES ("LION", "TRUE", 36);
+INSERT INTO bossggle (words_guessed, words_correct, words_score)
+VALUES ("TORGUE", "TRUE", 56);
+INSERT INTO bossggle (words_guessed, words_correct, words_score)
+VALUES ("TELLTALE", "TRUE", 72);
+INSERT INTO bossggle (words_guessed, words_correct, words_score)
+VALUES ("HI", "FALSE", 0);
+INSERT INTO bossggle (words_guessed, words_correct, words_score)
+VALUES ("BLEH", "TRUE", 36);
+INSERT INTO bossggle (words_guessed, words_correct, words_score)
+VALUES ("ARE", "TRUE", 27);
+INSERT INTO bossggle (words_guessed, words_correct, words_score)
+VALUES ("INTELLIGENCE", "TRUE", 108);
+
+SELECT words_guessed
+  FROM bossggle;
+
+SELECT COUNT(words_correct)
+  FROM bossggle
+ WHERE words_correct = "TRUE";
+
+SELECT COUNT(words_correct)
+  FROM bossggle
+ WHERE words_correct = "FALSE";
+
+SELECT words_guessed, words_score
+  FROM bossggle;
 
 SELECT SUM(words_score)
   FROM bossggle;
 
 SELECT *
-  FROM bossggle
- ORDER BY id DESC
- LIMIT 10;
+ FROM bossggle
+ORDER BY id DESC
+LIMIT 10;
